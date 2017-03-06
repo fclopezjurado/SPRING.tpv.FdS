b5509efd43861e0ca5c5ffd876a08543ef9a4206
Ceci <Ceci@192.168.1.129>
2017-03-06 21:12:33 +0100

    Se añaden la vista para el listado de productos

--- src/main/webapp/app/config.js
+++ src/main/webapp/app/config.js
@@ -26,6 +26,24 @@ tpv.config(function ($routeProvider) {
             controller: "DeleteAllController",
             controllerAs: "vm"
         })
+        //Feature 03 -01 
+    
+           .when("/feature03/feature03-1/listadoArticulos", {
+            templateUrl: "app/components/feature03/feature03-1/listadoArticulos.html",
+            controller: "",
+            controllerAs: "vm"
+        })
+        
+              .when("/feature03/feature03-1/listadoBordados", {
+            templateUrl: "app/components/feature03/feature03-1/listadoBordados.html",
+            controller: "",
+            controllerAs: "vm"
+        })
+              .when("/feature03/feature03-1/listadoPrendaImpresa", {
+            templateUrl: "app/components/feature03/feature03-1/listadoPrendaImpresa.html",
+            controller: "",
+            controllerAs: "vm"
+        })
         .otherwise({
             redirectTo: '/'
         });
