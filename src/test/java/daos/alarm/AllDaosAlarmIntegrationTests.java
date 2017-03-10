395bc10e7cbf701e2e9f41f1da586e31322a9105
danielgonzalezpro <daniel.glez1993@gmail.com>
2017-03-10 16:27:45 +0100

    Suite de pruebas para los Daos de todo lo que tenga que ver con las alarmas.

---
+++ src/test/java/daos/alarm/AllDaosAlarmIntegrationTests.java
@@ -0,0 +1,11 @@
+package daos.alarm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({AlarmDaoIT.class})
+public class AllDaosAlarmIntegrationTests {
+
+}
