a3347b2ac6aa1e3a53ffe8caea82a705f465a7dc
fonsi5 <alfonso.ayuso.guzman@gmail.com>
2017-03-11 17:41:21 +0100

    Cambios en capa Front y api para la creacion de un voucher

---
+++ src/main/webapp/app/components/feature07/createController.js
@@ -0,0 +1,35 @@
+tpv.controller('createController', [
+		'$timeout',
+		'f07Service',
+		function($timeout, f07Service) {
+			"use strict";
+			var vm = this;
+
+			vm.completed = false;
+			vm.error = false;
+			vm.response = "";
+			vm.value = 0;
+			vm.crear = crear;
+			vm.expiration = new Date();
+
+			function crear() {
+				const
+				delay = 2000;
+				f07Service.crear(vm.value, vm.expiration).then(
+						function(result) {
+							// promise was fullfilled
+							vm.completed = true;
+							vm.response = result.total;
+							$timeout(function() {
+								vm.completed = false;
+							}, delay)
+						}, function(errors) {
+							// handle errors
+							vm.error = true;
+							vm.response = errors;
+							$timeout(function() {
+								vm.error = false;
+							}, delay)
+						});
+			}
+		} ]);
