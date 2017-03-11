89b0ec490bd17eaa7d3e514e98de3ce147938769
fonsi5 <alfonso.ayuso.guzman@gmail.com>
2017-03-11 12:00:44 +0100

    cambios en front para el recurso de total en vales activos

---
+++ src/main/webapp/app/components/feature07/f07Service.js
@@ -0,0 +1,31 @@
+tpv.service('f07Service', ['$http', '$q', function ($http, $q) {
+   "use strict";
+
+   const urlBase="http://localhost:8080/SPRING.tpv.FdS.1.2.0-SNAPSHOT/api/v0";
+   
+   this.request = function(config) {
+	      let deferred = $q.defer();
+	      $http(config).then(function (response) {
+	    	  deferred.resolve(response.data);
+	      }, function (response){
+	    	  let errorMsg;
+	    	  if(response.data.error === undefined) {
+	    		  errorMsg="";
+	    	  }else{
+	    		  errorMsg = " --- " + response.data.error + ":" + response.data.description;
+	    	  }
+	    	  deferred.reject( 
+	    		 "Error (" + response.status + ":" + response.statusText + ")" + errorMsg );
+	      });
+	      return deferred.promise;	   
+   }
+
+   this.total = function () {
+      let config = {
+ 	     method: 'GET',
+ 	     url: urlBase + "/vouchers",
+	  };
+      return this.request(config);
+   }
+   
+}]);
