0ffe5ce9c6c9e894e8f58881382339caf258d241
bn0450 <bn0450@192.168.7.11>
2017-03-11 11:49:09 +0100

    Menús y controllers

--- src/main/webapp/app/config.js
+++ src/main/webapp/app/config.js
@@ -30,17 +30,17 @@ tpv.config(function ($routeProvider) {
 
        .when("/feature03/feature03-1/listadoArticulos", {
 	        templateUrl: "app/components/feature03/feature03-1/listadoArticulos.html",
-	        controller: "",
+	        controller: "listadoArticulosController",
 	        controllerAs: "vm"
         })
         .when("/feature03/feature03-1/listadoBordados", {
             templateUrl: "app/components/feature03/feature03-1/listadoBordados.html",
-            controller: "",
+            controller: "listadoBordadosControler",
             controllerAs: "vm"
         })
         .when("/feature03/feature03-1/listadoPrendaImpresa", {
             templateUrl: "app/components/feature03/feature03-1/listadoPrendaImpresa.html",
-            controller: "",
+            controller: "listadoPrendaImpresaController",
             controllerAs: "vm"
         })
         .when("/feature10/crear", {
