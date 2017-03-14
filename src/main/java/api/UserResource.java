7bf3f0cc3d9213feb71db224a96715ba57b80115
georgecardenas <Warpmodder@192.168.1.37>
2017-03-14 19:22:48 +0100

    New resources for User: getAllUsers, updateUser, deleteUser

--- src/main/java/api/UserResource.java
+++ src/main/java/api/UserResource.java
@@ -1,5 +1,8 @@
 package api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import api.exceptions.AlreadyExistUserFieldException;
 import api.exceptions.InvalidUserFieldException;
+import api.exceptions.NotFoundUserIdException;
 import controllers.UserController;
 import entities.users.Role;
+import wrappers.UserForEditWrapper;
 import wrappers.UserWrapper;
 
 @RestController
@@ -38,6 +43,27 @@ public class UserResource {
         if (!this.userController.registration(userWrapper, Role.CUSTOMER)) {
             throw new AlreadyExistUserFieldException();
         }
+    }
+    
+    @RequestMapping(value = Uris.USERS, method = RequestMethod.GET)
+    public List<UserForEditWrapper> getAllUsers() {
+        //TODO use instead of mock: this.userController.findAll();
+        List<UserForEditWrapper> list = new ArrayList<UserForEditWrapper>();
+        list.add(new UserForEditWrapper(66000000, "Prueba", true, "Calle prueba", "12345678Z", "prueba@mail.com", "25/10/2008"));
+        return list;
+    }
+    
+    @RequestMapping(value = Uris.USERS, method = RequestMethod.PUT)
+    public void updateUser(@RequestBody UserForEditWrapper userWrapper)
+            throws InvalidUserFieldException, NotFoundUserIdException {
+        validateField(userWrapper.getUsername(), "username");
+        //TODO use: this.userController.updateUser(userWrapper);
+    }
+    
+    @RequestMapping(value = Uris.USERS, method = RequestMethod.DELETE)
+    public void deleteUser(@RequestBody long mobile)
+            throws InvalidUserFieldException, NotFoundUserIdException {
+        //TODO use: this.userController.deleteUser(mobile);
     }
 
     private void validateField(String field, String msg) throws InvalidUserFieldException {
