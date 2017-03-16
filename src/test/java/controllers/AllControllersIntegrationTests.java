542b0cd82b34aea08d8e82cf600903b4cc24bdc1
Jose Maria <jotaememoreno@gmail.com>
2017-03-17 00:58:26 +0100

    And adding Test to IT suite

--- src/test/java/controllers/AllControllersIntegrationTests.java
+++ src/test/java/controllers/AllControllersIntegrationTests.java
@@ -1,11 +1,12 @@
 package controllers;
 
+import org.hibernate.stat.Statistics;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TokenControllerIT.class, ArticleControllerIT.class, AlarmControllerIT.class})
+@SuiteClasses({TokenControllerIT.class, ArticleControllerIT.class, AlarmControllerIT.class, Statistics.class})
 
 public class AllControllersIntegrationTests {
 
