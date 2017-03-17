e5df50a2532f2e00c1887137c3e9849fca8e243d
Jose Maria <jotaememoreno@gmail.com>
2017-03-17 01:02:10 +0100

    Refactoring

--- src/test/java/controllers/AllControllersIntegrationTests.java
+++ src/test/java/controllers/AllControllersIntegrationTests.java
@@ -1,12 +1,11 @@
 package controllers;
 
-import org.hibernate.stat.Statistics;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TokenControllerIT.class, ArticleControllerIT.class, AlarmControllerIT.class, Statistics.class})
+@SuiteClasses({TokenControllerIT.class, ArticleControllerIT.class, AlarmControllerIT.class, StatisticsControllerIT.class})
 
 public class AllControllersIntegrationTests {
 
