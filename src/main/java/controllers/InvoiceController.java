4a7391301f379310b05c65eb1096831b44b98996
Francisco López <fclopezjurado@gmail.com>
2017-03-18 12:33:36 +0100

    Fixes in "InvoiceController" controller class from "controllers" package to add "public InvoiceWrapper findByTicketReference(String ticketReference)" method.
    All methods in this class are not implemented yet.

--- src/main/java/controllers/InvoiceController.java
+++ src/main/java/controllers/InvoiceController.java
@@ -2,10 +2,17 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 
+import wrappers.InvoiceWrapper;
+
 @Controller
 public class InvoiceController {
 
-    public void createInvoice(long ticketID) {
+    public void createInvoice(String ticketReference) {
         // TODO Implement this method
+    }
+
+    public InvoiceWrapper findByTicketReference(String ticketReference) {
+        // TODO Implement this method
+        return new InvoiceWrapper(1L);
     }
 }
