69bdf5c58f287f5e7e2a6c061980ad42a3e9b7e1
Francisco López <fclopezjurado@gmail.com>
2017-03-18 11:05:19 +0100

    New "InvoiceResource" class from "api" package to define API endpoints for invoices

---
+++ src/main/java/api/InvoiceResource.java
@@ -0,0 +1,10 @@
+package api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Uris.VERSION + Uris.INVOICES)
+public class InvoiceResource {
+
+}
