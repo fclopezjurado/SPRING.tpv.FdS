6b1109394d081a0b335d579b1adb4f353447cf87
Francisco López <fclopezjurado@gmail.com>
2017-03-18 11:37:34 +0100

    Implementation of "InvoiceResource" API resource class from "api" package. An end-point to create a new invoice has been added.

--- src/main/java/api/InvoiceResource.java
+++ src/main/java/api/InvoiceResource.java
@@ -1,10 +1,39 @@
 package api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.exceptions.NotFoundTicketIdException;
+import controllers.InvoiceController;
+import controllers.TicketController;
 
 @RestController
 @RequestMapping(Uris.VERSION + Uris.INVOICES)
 public class InvoiceResource {
+
+    private InvoiceController invoiceController;
+
+    private TicketController ticketController;
+
+    @Autowired
+    public void setInvoiceController(InvoiceController invoiceController) {
+        this.invoiceController = invoiceController;
+    }
+
+    @Autowired
+    public void setTicketController(TicketController ticketController) {
+        this.ticketController = ticketController;
+    }
+
+    @RequestMapping(value = Uris.ID, method = RequestMethod.POST)
+    public void createInvoice(@PathVariable(value = "id") long ticketID) throws NotFoundTicketIdException {
+        if (!this.ticketController.ticketExists(ticketID))
+            throw new NotFoundTicketIdException();
+
+        this.invoiceController.createInvoice(ticketID);
+    }
 
 }
