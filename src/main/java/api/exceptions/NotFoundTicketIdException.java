80239daf90d17bd72230d1ca046827bdf0a170cb
Francisco López <fclopezjurado@gmail.com>
2017-03-18 11:19:22 +0100

    Implementation of "NotFoundTicketIdException" exception class from "api.exception" package.

--- src/main/java/api/exceptions/NotFoundTicketIdException.java
+++ src/main/java/api/exceptions/NotFoundTicketIdException.java
@@ -1,5 +1,19 @@
 package api.exceptions;
 
-public class NotFoundTicketIdException {
+public class NotFoundTicketIdException extends ApiException {
+
+    private static final long serialVersionUID = -1344640670884805385L;
+
+    public static final String DESCRIPTION = "No se encuentra el identificador de ticket utilizado";
+
+    public static final int CODE = 10;
+
+    public NotFoundTicketIdException() {
+        this("");
+    }
+
+    public NotFoundTicketIdException(String detail) {
+        super(DESCRIPTION + ". " + detail, CODE);
+    }
 
 }
