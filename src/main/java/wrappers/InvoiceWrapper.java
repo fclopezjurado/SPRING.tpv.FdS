8b705f7b7cd5d7be98678fb1704f2653c956747e
Francisco López <fclopezjurado@gmail.com>
2017-03-18 12:21:19 +0100

    Implementation of "InvoiceWrapper" wrapper class from "wrappers" package

--- src/main/java/wrappers/InvoiceWrapper.java
+++ src/main/java/wrappers/InvoiceWrapper.java
@@ -2,4 +2,23 @@ package wrappers;
 
 public class InvoiceWrapper {
 
+    private long ticket_id;
+
+    public InvoiceWrapper(long ticket_id) {
+        this.ticket_id = ticket_id;
+    }
+
+    public long getTicket_id() {
+        return ticket_id;
+    }
+
+    public void setTicket_id(long ticket_id) {
+        this.ticket_id = ticket_id;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceWrapper [ticket_id=" + ticket_id + "]";
+    }
+
 }
