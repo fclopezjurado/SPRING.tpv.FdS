5ed459597b0269199c81e5dad50efd4d86381e79
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:31:50 +0100

    Fixes in "InvoiceResource" class for API REST from "api" package to include changes in method from "createInvoice" end-point.

--- src/main/java/api/InvoiceResource.java
+++ src/main/java/api/InvoiceResource.java
@@ -1,15 +1,16 @@
 package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.exceptions.NotFoundTicketReferenceException;
 import controllers.InvoiceController;
 import controllers.TicketController;
 import wrappers.InvoiceWrapper;
+import wrappers.TicketWrapper;
 
 @RestController
 @RequestMapping(Uris.VERSION + Uris.INVOICES)
@@ -30,21 +31,11 @@ public class InvoiceResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void createInvoice(@RequestParam(value = "reference", required = true) String ticketReference)
-            throws NotFoundTicketReferenceException {
-        if (!this.ticketController.ticketExistsByReference(ticketReference))
+    public InvoiceWrapper createInvoice(@RequestBody TicketWrapper ticketWrapper) throws NotFoundTicketReferenceException {
+        if (!this.ticketController.ticketExistsByReference(ticketWrapper.getReference()))
             throw new NotFoundTicketReferenceException();
 
-        this.invoiceController.createInvoice(ticketReference);
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public InvoiceWrapper getInvoiceByTicketReference(@RequestParam(value = "reference", required = true) String ticketReference)
-            throws NotFoundTicketReferenceException {
-        if (!this.ticketController.ticketExistsByReference(ticketReference))
-            throw new NotFoundTicketReferenceException();
-
-        return this.invoiceController.findByTicketReference(ticketReference);
+        return this.invoiceController.createInvoice(ticketWrapper.getReference());
     }
 
 }
