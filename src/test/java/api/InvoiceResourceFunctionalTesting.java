853eadd619cc3a940b2e4865266c182fe064d138
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:36:33 +0100

    Implementation of "InvoiceResourceFunctionalTesting" class for tests from "test.api" package. Two test from "createInvoice" end-point has been implemented.
    The first is to test the end-point functionality and second is to test
    the same end-point when it throws exceptions.

--- src/test/java/api/InvoiceResourceFunctionalTesting.java
+++ src/test/java/api/InvoiceResourceFunctionalTesting.java
@@ -1,5 +1,62 @@
 package api;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+import config.PersistenceConfig;
+import config.TestsPersistenceConfig;
+import wrappers.InvoiceWrapper;
+import wrappers.TicketWrapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class})
 public class InvoiceResourceFunctionalTesting {
+
+    public static final String ticketReferenceWrong = "DW7yzcVpRLguAs";
+
+    @Before
+    public void seedDataBase() {
+        new RestService().deleteAll();
+        new RestService().seedDatabase();
+    }
+
+    @Test
+    public void testCreateInvoice() {
+        List<TicketWrapper> tickets = Arrays
+                .asList(new RestBuilder<TicketWrapper[]>(RestService.URL).path(Uris.TICKETS).clazz(TicketWrapper[].class).get().build());
+        InvoiceWrapper invoice = new RestBuilder<InvoiceWrapper>(RestService.URL).path(Uris.INVOICES).body(tickets.get(0))
+                .clazz(InvoiceWrapper.class).post().build();
+
+        assertEquals(invoice.getTicket_id(), tickets.get(0).getId());
+    }
+
+    @Test
+    public void testCreateInvoiceException() {
+        TicketWrapper ticketForRequest = new TicketWrapper();
+        ticketForRequest.setReference(ticketReferenceWrong);
+
+        try {
+            new RestBuilder<InvoiceWrapper>(RestService.URL).path(Uris.INVOICES).body(ticketForRequest).clazz(InvoiceWrapper.class).post()
+                    .build();
+        } catch (HttpClientErrorException httpError) {
+            assertEquals(HttpStatus.NOT_FOUND, httpError.getStatusCode());
+        }
+    }
+
+    @After
+    public void after() {
+        new RestService().deleteAll();
+    }
 
 }
