d1950321a42a70e11633746cca9f67da02867c92
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:26:31 +0100

    Fixes in "TicketController" controller class from "controllers" package to:
    1.- Add a new attribute called "ticketDao" with an instance of
    "TicketDao" class to use the dao for tickets.
    2.- Add a new method called "findAll()" to get all tickets in the
    system, using "ticketDao" attribute.
    3.- Add a new method called "ticketExistsByReference()" to know if there
    is a ticket in the system with a reference equal to reference inserted
    in method parameter.

--- src/main/java/controllers/TicketController.java
+++ src/main/java/controllers/TicketController.java
@@ -1,10 +1,36 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import daos.core.TicketDao;
+import entities.core.Ticket;
 import wrappers.TicketWrapper;
 
 @Controller
 public class TicketController {
+
+    @Autowired
+    private TicketDao ticketDao;
+
+    @Autowired
+    public void setTicketDao(TicketDao ticketDao) {
+        this.ticketDao = ticketDao;
+    }
+
+    public List<TicketWrapper> findAll() {
+        List<Ticket> tickets = ticketDao.findAll();
+        List<TicketWrapper> ticketsWrapper = new ArrayList<TicketWrapper>();
+
+        for (Ticket ticket : tickets)
+            ticketsWrapper.add(new TicketWrapper(ticket.getId(), ticket.getCreated(), ticket.getReference(), ticket.getTicketState(),
+                    ticket.getShoppingList(), ticket.getUser()));
+
+        return ticketsWrapper;
+    }
 
     public TicketWrapper getTicketByReference(String reference) {
         // TODO Implement this method
@@ -25,8 +51,12 @@ public class TicketController {
     }
 
     public boolean ticketExistsByReference(String ticketReference) {
-        // TODO Implement this method
-        return true;
+        Ticket ticket = ticketDao.findByReference(ticketReference);
+        
+        if (ticket != null)
+            return ticket.getReference().equals(ticketReference);
+        
+        return false;
     }
 
 }
