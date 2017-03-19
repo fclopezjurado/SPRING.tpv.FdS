bb0e2e696437665e7b20a44234ba936c1ad89dd7
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:34:04 +0100

    Fixes in "InvoiceController" controller class from "controllers" package to:
    1.- Add a new attribute called "invoiceDao" with an instance of
    "InvoiceDao" class to use the dao for invoices.
    2.- Implementation of "createInvoice()" and "invoiceExists()" methods.

--- src/main/java/controllers/InvoiceController.java
+++ src/main/java/controllers/InvoiceController.java
@@ -1,18 +1,53 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import daos.core.InvoiceDao;
+import daos.core.TicketDao;
+import entities.core.Invoice;
+import entities.core.Ticket;
 import wrappers.InvoiceWrapper;
 
 @Controller
 public class InvoiceController {
 
-    public void createInvoice(String ticketReference) {
-        // TODO Implement this method
+    private static final int FIRST_INVOICE_ID = 1;
+
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Autowired
+    private TicketDao ticketDao;
+
+    @Autowired
+    public void setInvoiceDao(InvoiceDao invoiceDao) {
+        this.invoiceDao = invoiceDao;
     }
 
-    public InvoiceWrapper findByTicketReference(String ticketReference) {
-        // TODO Implement this method
-        return new InvoiceWrapper(1L);
+    @Autowired
+    public void setTicketDao(TicketDao ticketDao) {
+        this.ticketDao = ticketDao;
     }
+
+    public InvoiceWrapper createInvoice(String ticketReference) {
+        Ticket ticket = this.ticketDao.findByReference(ticketReference);
+        int newInvoiceID = this.invoiceDao.findMaxInvoiceID();
+
+        if (newInvoiceID < FIRST_INVOICE_ID)
+            newInvoiceID = FIRST_INVOICE_ID;
+        else
+            newInvoiceID++;
+
+        Invoice invoice = new Invoice(newInvoiceID, ticket);
+        this.invoiceDao.save(invoice);
+
+        return new InvoiceWrapper(newInvoiceID, ticket.getId());
+    }
+
+    public boolean invoiceExists(int id) {
+        Invoice invoice = invoiceDao.findOne(id);
+        return invoice.getId() == id;
+    }
+
 }
