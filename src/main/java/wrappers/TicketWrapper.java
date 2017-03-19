61629fc9697f9829c1b39c9d515ddb2b5928b034
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:11:21 +0100

    Fixes in "TicketWrapper" wrapper from "wrappers" package for tickets to:
    1.- Add an empty constructor.
    2.- Add an "long" parameter to not empty constructor to initialize "id"
    instance attribute .

--- src/main/java/wrappers/TicketWrapper.java
+++ src/main/java/wrappers/TicketWrapper.java
@@ -21,7 +21,11 @@ public class TicketWrapper {
 
     private User user;
 
-    private TicketWrapper(Calendar created, String reference, TicketState ticketState, List<Shopping> shoppingList, User user) {
+    public TicketWrapper() {
+    }
+
+    public TicketWrapper(long id, Calendar created, String reference, TicketState ticketState, List<Shopping> shoppingList, User user) {
+        this.id = id;
         this.created = created;
         this.reference = reference;
         this.ticketState = ticketState;
