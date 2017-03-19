899da40edabd2c5401c2a70e284064445480aae2
Francisco López <fclopezjurado@gmail.com>
2017-03-19 16:08:15 +0100

    Fixes in "InvoiceWrapper" wrapper from "wrappers" package for invoices, to add a new attribute to store an invoice id.

--- src/main/java/wrappers/InvoiceWrapper.java
+++ src/main/java/wrappers/InvoiceWrapper.java
@@ -2,10 +2,24 @@ package wrappers;
 
 public class InvoiceWrapper {
 
+    private int id;
+
     private long ticket_id;
 
-    public InvoiceWrapper(long ticket_id) {
+    public InvoiceWrapper() {
+    }
+
+    public InvoiceWrapper(int id, long ticket_id) {
+        this.id = id;
         this.ticket_id = ticket_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public long getTicket_id() {
@@ -18,7 +32,7 @@ public class InvoiceWrapper {
 
     @Override
     public String toString() {
-        return "InvoiceWrapper [ticket_id=" + ticket_id + "]";
+        return "InvoiceWrapper [id=" + id + ", ticket_id=" + ticket_id + "]";
     }
 
 }
