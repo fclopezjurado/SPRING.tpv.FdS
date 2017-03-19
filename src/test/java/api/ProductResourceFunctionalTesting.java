31488d8dbffc16d6157ab912fd4a52d7fe46f480
eduardofranco <eduardofranco@localhost>
2017-03-19 09:33:56 +0100

    Incorporacion en el allFunctionalTesting

--- src/test/java/api/ProductResourceFunctionalTesting.java
+++ src/test/java/api/ProductResourceFunctionalTesting.java
@@ -7,16 +7,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import config.PersistenceConfig;
-import config.TestsPersistenceConfig;
-import wrappers.ArticleWrapper;
+
 import wrappers.ProductFilterWrapper;
 import wrappers.ProductsOutFilterWrapper;
-import wrappers.ProviderWrapper;
+
 
 
 public class ProductResourceFunctionalTesting {
