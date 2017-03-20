ae13c431a8cb23ec5257c1542c4279f926b7ba74
Francisco López <fclopezjurado@gmail.com>
2017-03-20 14:00:53 +0100

    Fixes in "InvoiceResource" API class from "api" package to add "getInvoicesByUserMobile" end-point to return all invoices by a user mobile number.

--- src/main/java/api/InvoiceResource.java
+++ src/main/java/api/InvoiceResource.java
@@ -1,15 +1,19 @@
 package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.exceptions.NotFoundTicketReferenceException;
+import api.exceptions.NotFoundUserMobileException;
 import controllers.InvoiceController;
 import controllers.TicketController;
+import controllers.UserController;
 import wrappers.InvoiceWrapper;
+import wrappers.InvoicesWrapper;
 import wrappers.TicketWrapper;
 
 @RestController
@@ -19,6 +23,8 @@ public class InvoiceResource {
     private InvoiceController invoiceController;
 
     private TicketController ticketController;
+
+    private UserController userController;
 
     @Autowired
     public void setInvoiceController(InvoiceController invoiceController) {
@@ -30,6 +36,11 @@ public class InvoiceResource {
         this.ticketController = ticketController;
     }
 
+    @Autowired
+    public void setUserController(UserController userController) {
+        this.userController = userController;
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public InvoiceWrapper createInvoice(@RequestBody TicketWrapper ticketWrapper) throws NotFoundTicketReferenceException {
         if (!this.ticketController.ticketExistsByReference(ticketWrapper.getReference()))
@@ -38,4 +49,11 @@ public class InvoiceResource {
         return this.invoiceController.createInvoice(ticketWrapper.getReference());
     }
 
+    @RequestMapping(value = Uris.ID, method = RequestMethod.GET)
+    public InvoicesWrapper getInvoicesByUserMobile(@PathVariable(value = "id") long userMobile) throws NotFoundUserMobileException {
+        if (!this.userController.userExistsByMobile(userMobile))
+            throw new NotFoundUserMobileException();
+
+        return this.invoiceController.getInvoicesByUserMobile(userMobile);
+    }
 }
