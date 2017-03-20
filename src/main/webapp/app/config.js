f806abcca8b40212492747fd5af09a68a1a8f216
Enrique Arranz <enrique.arranz@urjc.es>
2017-03-20 17:27:45 +0100

    fix route

--- src/main/webapp/app/config.js
+++ src/main/webapp/app/config.js
@@ -104,6 +104,11 @@ tpv.config(function ($routeProvider) {
             controller: "CashierBalancingController",
             controllerAs: "vm"
         })
+        .when("/feature09/cierrecaja", {
+            templateUrl: "app/components/feature09/list_cashier_balancing.html",
+            controller: "CashierBalancingController",
+            controllerAs: "vm"
+        })
         .when("/feature11/ticket", {
             templateUrl: "app/components/feature11/ticket.html",
             controller: "ProductsServiceController",
