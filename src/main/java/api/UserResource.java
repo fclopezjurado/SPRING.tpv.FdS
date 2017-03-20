a1cce8eebc840d6494e59eaa792af64796ec0b8d
Francisco López <fclopezjurado@gmail.com>
2017-03-20 15:49:03 +0100

    Fixes in "UserResource" API class from "api" package to add "getByMobile" end-point to get a user registered in the system using a mobile number.

--- src/main/java/api/UserResource.java
+++ src/main/java/api/UserResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import api.exceptions.AlreadyExistUserFieldException;
 import api.exceptions.InvalidUserFieldException;
 import api.exceptions.NotFoundUserIdException;
+import api.exceptions.NotFoundUserMobileException;
 import controllers.UserController;
 import entities.users.Role;
 import wrappers.UserForEditListWrapper;
@@ -45,32 +47,38 @@ public class UserResource {
             throw new AlreadyExistUserFieldException();
         }
     }
-    
+
     @RequestMapping(value = Uris.USERS, method = RequestMethod.GET)
     public UserForEditListWrapper getAllUsers() {
-        //TODO use instead of mock: this.userController.findAll();
+        // TODO use instead of mock: this.userController.findAll();
         List<UserForEditWrapper> list = new ArrayList<UserForEditWrapper>();
         list.add(new UserForEditWrapper(66000000, "Prueba", true, "Calle prueba", "12345678Z", "prueba@mail.com", "25/10/2008"));
         return new UserForEditListWrapper(list);
     }
-    
+
     @RequestMapping(value = Uris.USERS, method = RequestMethod.PUT)
-    public void updateUser(@RequestBody UserForEditWrapper userWrapper)
-            throws InvalidUserFieldException, NotFoundUserIdException {
+    public void updateUser(@RequestBody UserForEditWrapper userWrapper) throws InvalidUserFieldException, NotFoundUserIdException {
         validateField(userWrapper.getUsername(), "username");
-        //TODO use: this.userController.updateUser(userWrapper);
+        // TODO use: this.userController.updateUser(userWrapper);
     }
-    
+
     @RequestMapping(value = Uris.USERS, method = RequestMethod.DELETE)
-    public void deleteUser(@RequestBody long mobile)
-            throws InvalidUserFieldException, NotFoundUserIdException {
-        //TODO use: this.userController.deleteUser(mobile);
+    public void deleteUser(@RequestBody long mobile) throws InvalidUserFieldException, NotFoundUserIdException {
+        // TODO use: this.userController.deleteUser(mobile);
     }
 
     private void validateField(String field, String msg) throws InvalidUserFieldException {
         if (field == null || field.isEmpty()) {
             throw new InvalidUserFieldException(msg);
         }
+    }
+
+    @RequestMapping(value = Uris.ID, method = RequestMethod.GET)
+    public UserWrapper getByMobile(@PathVariable(value = "id") long mobile) throws NotFoundUserMobileException {
+        if (!this.userController.userExistsByMobile(mobile))
+            throw new NotFoundUserMobileException();
+
+        return this.userController.getByMobile(mobile);
     }
 
 }
