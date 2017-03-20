d35b2f0fe1a2d844a24c7ec3f4ec7bdb248af1d8
Francisco López <fclopezjurado@gmail.com>
2017-03-20 13:29:54 +0100

    Implementation of "InvoicesWrapper" wrapper class from "wrappers" package.

--- src/main/java/wrappers/InvoicesWrapper.java
+++ src/main/java/wrappers/InvoicesWrapper.java
@@ -1,5 +1,31 @@
 package wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InvoicesWrapper {
+
+    private List<InvoiceWrapper> invoices;
+
+    public InvoicesWrapper() {
+        this.invoices = new ArrayList<InvoiceWrapper>();
+    }
+
+    public InvoicesWrapper(List<InvoiceWrapper> invoices) {
+        this.invoices = invoices;
+    }
+
+    public List<InvoiceWrapper> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<InvoiceWrapper> invoices) {
+        this.invoices = invoices;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoicesWrapper [invoices=" + invoices + "]";
+    }
 
 }
