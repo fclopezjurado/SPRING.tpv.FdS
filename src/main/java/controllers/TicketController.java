ab024bc25b77090ab42272198f7fff295e361043
albertobricio <albertobricioortiz@gmail.com>
2017-03-20 12:21:03 +0100

    TicketDao - include ticketfindbyid method TicketController - include method getticketbyreference and createticket TicketControllerIT class created and included test for controller methods getticketbyreference and createticket Reference included in ALLControllersIntegrationTests

--- src/main/java/controllers/TicketController.java
+++ src/main/java/controllers/TicketController.java
@@ -1,20 +1,30 @@
 package controllers;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import daos.core.TicketDao;
+import entities.core.Shopping;
 import entities.core.Ticket;
+import entities.core.TicketState;
+import entities.users.User;
 import wrappers.TicketWrapper;
 
 @Controller
 public class TicketController {
 
+    private final int PADDING = 10000;
+
     @Autowired
     private TicketDao ticketDao;
+
+    private Ticket lastTicket;
 
     @Autowired
     public void setTicketDao(TicketDao ticketDao) {
@@ -25,23 +35,43 @@ public class TicketController {
         List<Ticket> tickets = ticketDao.findAll();
         List<TicketWrapper> ticketsWrapper = new ArrayList<TicketWrapper>();
 
-        for (Ticket ticket : tickets)
+        for (Ticket ticket : tickets) {
             ticketsWrapper.add(new TicketWrapper(ticket.getId(), ticket.getCreated(), ticket.getReference(), ticket.getTicketState(),
                     ticket.getShoppingList(), ticket.getUser()));
+        }
 
         return ticketsWrapper;
     }
 
     public TicketWrapper getTicketByReference(String reference) {
-        // TODO Implement this method
+        Ticket ticket = ticketDao.findByReference(reference);
+
+        TicketWrapper ticketWrapper = new TicketWrapper(ticket.getId(), ticket.getCreated(), ticket.getReference(), ticket.getTicketState(),
+                ticket.getShoppingList(), ticket.getUser());
         // TODO Feature 11: If ticket state is Committed no Wrapper will be returned
-        return null;
+        return (ticketWrapper);
     }
 
-    public TicketWrapper createTicket(TicketWrapper ticketWrapper) {
-        // TODO Implement this method
+    public TicketWrapper createTicket(List<Shopping> shoppingList, User user) {
+        Ticket ticket;
+        TicketWrapper ticketWrapper;
+        if (user == null) {
+            ticket = new Ticket(getNextTicketId(), TicketState.COMMITTED);
+        } else {
+            ticket = new Ticket(getNextTicketId(), TicketState.OPENED);
+            ticket.setUser(user);
+        }
+        for (Shopping shopping : shoppingList) {
+            ticket.addShopping(shopping);
+        }
+
+        ticketDao.save(ticket);
+
+        ticketWrapper = new TicketWrapper(ticket.getId(), ticket.getCreated(), ticket.getReference(), ticket.getTicketState(),
+                ticket.getShoppingList(), ticket.getUser());
+
         // TODO Feature 11: Send an email after creation
-        return null;
+        return ticketWrapper;
     }
 
     public TicketWrapper updateTicket(TicketWrapper ticketWrapper) {
@@ -52,11 +82,40 @@ public class TicketController {
 
     public boolean ticketExistsByReference(String ticketReference) {
         Ticket ticket = ticketDao.findByReference(ticketReference);
-        
-        if (ticket != null)
+
+        if (ticket != null) {
             return ticket.getReference().equals(ticketReference);
-        
+        }
+
         return false;
+    }
+
+    public long getNextTicketId() {
+        lastTicket = ticketDao.findFirstByOrderByIdDesc();
+        long firstTicketofDay = getFirstTicketOfDay();
+
+        if (firstTicketofDay > lastTicket.getId()) {
+            return firstTicketofDay;
+        } else {
+            return (lastTicket.getId() + 1);
+        }
+    }
+
+    public long getFirstTicketOfDay() {
+        long todayFormated = getTodayUSformat();
+        long firstTicketofDay = (todayFormated * PADDING) + 1;
+        return firstTicketofDay;
+    }
+
+    public long getTodayUSformat() {
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        Long todayFormated = Long.parseLong(formatter.format(today));
+        return todayFormated;
+    }
+
+    public Ticket getLastTicket() {
+        return lastTicket;
     }
 
 }
