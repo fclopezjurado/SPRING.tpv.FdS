63ec0477758a4cfbee09688a2c1148ddf14e0a1e
Francisco López <fclopezjurado@gmail.com>
2017-03-20 13:38:55 +0100

    Implementation of "NotFoundUserMobileException" exception class from "exceptions" package.

--- src/main/java/api/exceptions/NotFoundUserMobileException.java
+++ src/main/java/api/exceptions/NotFoundUserMobileException.java
@@ -1,5 +1,19 @@
 package api.exceptions;
 
-public class NotFoundUserMobileException {
+public class NotFoundUserMobileException extends ApiException {
+
+    private static final long serialVersionUID = -1344640670884805385L;
+
+    public static final String DESCRIPTION = "No se encuentra ninguno usuario en el sistema, a partir del número de teléfono utilizado";
+
+    public static final int CODE = 11;
+
+    public NotFoundUserMobileException() {
+        this("");
+    }
+
+    public NotFoundUserMobileException(String detail) {
+        super(DESCRIPTION + ". " + detail, CODE);
+    }
 
 }
