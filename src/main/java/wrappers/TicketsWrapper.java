698b99e32bbc38bac93c08ad2a39c2fcc3815d59
Francisco López <fclopezjurado@gmail.com>
2017-03-21 22:56:17 +0100

    Implementation of "TicketsWrapper" wrapper class from "wrappers" package.

--- src/main/java/wrappers/TicketsWrapper.java
+++ src/main/java/wrappers/TicketsWrapper.java
@@ -1,5 +1,38 @@
 package wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketsWrapper {
+
+    private List<TicketWrapper> tickets;
+
+    public TicketsWrapper() {
+        this.tickets = new ArrayList<TicketWrapper>();
+    }
+
+    public List<TicketWrapper> getTickets() {
+        return this.tickets;
+    }
+
+    public boolean isEmpty() {
+        return this.tickets.isEmpty();
+    }
+
+    public TicketWrapper getFirstTicket() {
+        if (this.tickets.isEmpty())
+            return new TicketWrapper();
+
+        return this.tickets.get(0);
+    }
+
+    public void addTicketWrapper(TicketWrapper ticket) {
+        this.tickets.add(ticket);
+    }
+
+    @Override
+    public String toString() {
+        return "TicketsWrapper [tickets=" + tickets + "]";
+    }
 
 }
