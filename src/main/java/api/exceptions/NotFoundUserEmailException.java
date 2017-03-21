bcbf0090a2a2c3817dec4f4b7285b348fe8eb357
Francisco López <fclopezjurado@gmail.com>
2017-03-21 23:40:21 +0100

    Implementation of "NotFoundUserEmailException" exception class from "api.exceptions" package.

--- src/main/java/api/exceptions/NotFoundUserEmailException.java
+++ src/main/java/api/exceptions/NotFoundUserEmailException.java
@@ -1,5 +1,19 @@
 package api.exceptions;
 
-public class NotFoundUserEmailException {
+public class NotFoundUserEmailException extends ApiException {
+
+    private static final long serialVersionUID = -1344640670884805385L;
+
+    public static final String DESCRIPTION = "No se encuentra ninguno usuario en el sistema, a partir del correo electrónico utilizado";
+
+    public static final int CODE = 13;
+
+    public NotFoundUserEmailException() {
+        this("");
+    }
+
+    public NotFoundUserEmailException(String detail) {
+        super(DESCRIPTION + ". " + detail, CODE);
+    }
 
 }
