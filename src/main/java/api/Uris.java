87a779cbf704bc1bc3415cab4a2600be7ae6ec1e
Francisco López <fclopezjurado@gmail.com>
2017-03-22 00:18:06 +0100

    Fixes in "Uris" class from "api" package to delete "USER_EMAIL_PATH_VARIABLE" URI because it's unnecessary.

--- src/main/java/api/Uris.java
+++ src/main/java/api/Uris.java
@@ -63,9 +63,7 @@ public class Uris {
     public static final String FAMILY_NAME = "/{name}";
 
     public static final String USER_MOBILE_PATH = "/mobile";
-
+    
     public static final String USER_EMAIL_PATH = "/email";
-
-    public static final String USER_EMAIL_PATH_VARIABLE = "/{email}";
-
+    
 }
