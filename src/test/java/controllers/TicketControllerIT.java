4b3af325799893eb30335eea1a5e087fb3d4e335
albertobricio <albertobricioortiz@gmail.com>
2017-03-21 23:52:57 +0100

    testCreateOpenedTicket & testCreateAndUpdateTicket implemented in TicketControllerIT

--- src/test/java/controllers/TicketControllerIT.java
+++ src/test/java/controllers/TicketControllerIT.java
@@ -19,10 +19,12 @@ import config.TestsControllerConfig;
 import config.TestsPersistenceConfig;
 import daos.core.ArticleDao;
 import daos.core.TicketDao;
+import daos.users.UserDao;
 import entities.core.Product;
 import entities.core.Shopping;
 import entities.core.Ticket;
 import entities.core.TicketState;
+import entities.users.User;
 import wrappers.TicketWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -38,11 +40,17 @@ public class TicketControllerIT {
     @Autowired
     private ArticleDao articleDao;
 
+    @Autowired
+    private UserDao userDao;
+
     Ticket ticket;
+
+    User user;
 
     @Before
     public void setup() {
         ticket = ticketDao.findById(1L);
+        user = userDao.findByMobile(666000000);
         LogManager.getLogger(this.getClass()).info(ticket.getReference());
     }
 
@@ -65,5 +73,39 @@ public class TicketControllerIT {
         ticketWrapper = ticketController.createTicket(shoppingList, null);
 
         assertEquals(ticketWrapper.getTicketState(), TicketState.COMMITTED);
+    }
+
+    @Test
+    public void testCreateOpenedTicket() {
+        List<Shopping> shoppingList = new ArrayList<>();
+        TicketWrapper ticketWrapper;
+        for (int i = 0; i < 2; i++) {
+            Product product = articleDao.findOne(84000001111L + i);
+            shoppingList.add(new Shopping(3 + i, 0, product.getId(), product.getDescription(), product.getRetailPrice()));
+        }
+        ticketWrapper = ticketController.createTicket(shoppingList, user);
+
+        assertEquals(ticketWrapper.getTicketState(), TicketState.OPENED);
+    }
+
+    @Test
+    public void testCreateAndUpdateTicket() {
+        List<Shopping> shoppingList = new ArrayList<>();
+        TicketWrapper ticketWrapper;
+        for (int i = 0; i < 3; i++) {
+            Product product = articleDao.findOne(84000001111L + i);
+            shoppingList.add(new Shopping(4 + i, 0, product.getId(), product.getDescription(), product.getRetailPrice()));
+        }
+        ticketWrapper = ticketController.createTicket(shoppingList, user);
+
+        assertEquals(ticketWrapper.getTicketState(), TicketState.OPENED);
+
+        ticketWrapper.setTicketState(TicketState.STARTED);
+        ticketWrapper.getShoppingList().remove(0);
+
+        ticketWrapper = ticketController.updateTicket(ticketWrapper);
+
+        assertEquals(ticketWrapper.getTicketState(), TicketState.STARTED);
+        assertEquals(2, ticketWrapper.getShoppingList().size());
     }
 }
