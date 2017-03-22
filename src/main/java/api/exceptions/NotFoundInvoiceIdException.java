510acb3bb99508d70bbf79f7a7e9f7c99d0c1d35
Francisco López <fclopezjurado@gmail.com>
2017-03-23 00:14:45 +0100

    Implementation of "NotFoundInvoiceIdException" exception class from "api.exceptions" package.

--- src/main/java/api/exceptions/NotFoundInvoiceIdException.java
+++ src/main/java/api/exceptions/NotFoundInvoiceIdException.java
@@ -1,5 +1,19 @@
 package api.exceptions;
 
-public class NotFoundInvoiceIdException {
+public class NotFoundInvoiceIdException extends ApiException {
+
+    private static final long serialVersionUID = -1344640670884805385L;
+
+    public static final String DESCRIPTION = "No se encuentra el identificador de la factura utilizado";
+
+    public static final int CODE = 13;
+
+    public NotFoundInvoiceIdException() {
+        this("");
+    }
+
+    public NotFoundInvoiceIdException(String detail) {
+        super(DESCRIPTION + ". " + detail, CODE);
+    }
 
 }
