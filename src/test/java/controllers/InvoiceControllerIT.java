ba3cdf5e3b14dbb329ebe612f92e26351a7f2b1c
Francisco López <fclopezjurado@gmail.com>
2017-03-23 23:04:18 +0100

    Fixes in "InvoiceControllerIT" class for test from "test.controllers" package to add "testGetInvoicesByUserMobile" and "testGetInvoicesByUserMobileException" tests

--- src/test/java/controllers/InvoiceControllerIT.java
+++ src/test/java/controllers/InvoiceControllerIT.java
@@ -9,18 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import api.exceptions.NotFoundInvoiceIdException;
 import api.exceptions.NotFoundTicketReferenceException;
+import api.exceptions.NotFoundUserMobileException;
 import config.PersistenceConfig;
 import config.TestsControllerConfig;
 import config.TestsPersistenceConfig;
 import wrappers.InvoiceWrapper;
+import wrappers.InvoicesWrapper;
 import wrappers.TicketsWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class, TestsControllerConfig.class})
 public class InvoiceControllerIT {
 
-    private static final String WRONG_TICKET_REFERENCE = "wg_yXs1LJmSvNsld-aXBg27P1jA";
+    private static final String INVALID_TICKET_REFERENCE = "wg_yXs1LJmSvNsld-aXBg27P1jA";
+
+    private static final long VALID_USER_MOBILE = 666000000;
+
+    private static final long INVALID_USER_MOBILE = 666666666;
+
+    private static final int INVALID_INVOICE_ID = 999;
 
     @Autowired
     private InvoiceController invoiceController;
@@ -44,9 +53,53 @@ public class InvoiceControllerIT {
     @Test
     public void testCreateInvoiceWithWrongTicketReference() {
         try {
-            this.invoiceController.createInvoice(WRONG_TICKET_REFERENCE);
+            this.invoiceController.createInvoice(INVALID_TICKET_REFERENCE);
             fail();
         } catch (NotFoundTicketReferenceException exception) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testInvoiceExists() {
+        try {
+            InvoicesWrapper invoices = this.invoiceController.getInvoicesByUserMobile(VALID_USER_MOBILE);
+            assertEquals(false, invoices.isEmpty());
+            assertTrue(this.invoiceController.invoiceExists(invoices.getFirstInvoice().getId()));
+        } catch (NotFoundInvoiceIdException exception) {
+            LogManager.getLogger(this.getClass()).info("testInvoiceExists was wrong:  " + exception.getMessage());
+            fail();
+        } catch (NotFoundUserMobileException exception) {
+            LogManager.getLogger(this.getClass()).info("testInvoiceExists was wrong:  " + exception.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void testInvoiceExistsException() {
+        try {
+            this.invoiceController.invoiceExists(INVALID_INVOICE_ID);
+        } catch (NotFoundInvoiceIdException exception) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testGetInvoicesByUserMobile() {
+        try {
+            InvoicesWrapper invoices = this.invoiceController.getInvoicesByUserMobile(VALID_USER_MOBILE);
+            assertEquals(false, invoices.isEmpty());
+        } catch (NotFoundUserMobileException exception) {
+            LogManager.getLogger(this.getClass()).info("testInvoiceExists was wrong:  " + exception.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetInvoicesByUserMobileException() {
+        try {
+            this.invoiceController.getInvoicesByUserMobile(INVALID_USER_MOBILE);
+        } catch (NotFoundUserMobileException exception) {
             assertTrue(true);
         }
     }
