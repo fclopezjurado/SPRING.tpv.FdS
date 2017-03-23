ba342ca628bbf09b9334c93be96c25217539ef11
georgecardenas <Warpmodder@192.168.1.34>
2017-03-23 18:57:13 +0100

    update BudgetResource

--- src/main/java/api/BudgetResource.java
+++ src/main/java/api/BudgetResource.java
@@ -2,14 +2,15 @@ package api;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import controllers.BudgetController;
+import wrappers.BudgetDetailWrapper;
 import wrappers.BudgetListWrapper;
-import wrappers.BudgetWrapper;
 
 @RestController
 @RequestMapping(Uris.VERSION)
@@ -31,10 +32,20 @@ public class BudgetResource {
     public BudgetListWrapper getBudgets() {
         return budgetController.findAll();
     }
+    
+    @RequestMapping(value = Uris.BUDGETS, method = RequestMethod.GET)
+    public BudgetDetailWrapper getBudgetsByReference(@PathVariable String reference) {
+        return budgetController.getBudgetByReference(reference);
+    }
 
     @RequestMapping(value = Uris.BUDGETS, method = RequestMethod.DELETE)
-    public void deleteUser(@RequestBody String reference) {
+    public void deleteBudget(@RequestBody String reference) {
         budgetController.deleteBudget(reference);
+    }
+    
+    @RequestMapping(value = Uris.BUDGETS, method = RequestMethod.PUT)
+    public void updateBudget(BudgetDetailWrapper budgetWrapper) {
+        //BudgetController.updateBudget(budgetWrapper);
     }
 
 }
