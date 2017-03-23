be3b9bfefca29e299c82301ff64c42ec1e011a48
Francisco López <fclopezjurado@gmail.com>
2017-03-23 22:17:35 +0100

    Implementation of "InvoiceControllerIT" class for tests from "test.controllers" package. The following tests are added:
    1.- testCreateInvoice.
    2.- testCreateInvoiceException.

--- src/test/java/controllers/InvoiceControllerIT.java
+++ src/test/java/controllers/InvoiceControllerIT.java
@@ -1,5 +1,54 @@
 package controllers;
 
+import static org.junit.Assert.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import api.exceptions.NotFoundTicketReferenceException;
+import config.PersistenceConfig;
+import config.TestsControllerConfig;
+import config.TestsPersistenceConfig;
+import wrappers.InvoiceWrapper;
+import wrappers.TicketsWrapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class, TestsControllerConfig.class})
 public class InvoiceControllerIT {
+
+    private static final String WRONG_TICKET_REFERENCE = "wg_yXs1LJmSvNsld-aXBg27P1jA";
+
+    @Autowired
+    private InvoiceController invoiceController;
+
+    @Autowired
+    private TicketController ticketController;
+
+    @Test
+    public void testCreateInvoice() {
+        TicketsWrapper ticketsWrapper = this.ticketController.findAll();
+
+        try {
+            InvoiceWrapper invoiceWrapper = this.invoiceController.createInvoice(ticketsWrapper.getFirstTicket().getReference());
+            assertEquals(ticketsWrapper.getFirstTicket().getReference(), invoiceWrapper.getTicketReference());
+        } catch (NotFoundTicketReferenceException exception) {
+            LogManager.getLogger(this.getClass()).info("testCreateInvoice was wrong:  " + exception.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void testCreateInvoiceWithWrongTicketReference() {
+        try {
+            this.invoiceController.createInvoice(WRONG_TICKET_REFERENCE);
+            fail();
+        } catch (NotFoundTicketReferenceException exception) {
+            assertTrue(true);
+        }
+    }
 
 }
