9a3371a346188cbe648d22a5d4a4306028cb2042
eduardofranco <eduardofranco@localhost>
2017-03-25 20:05:35 +0100

    Incorporacion de Daos y test de Emroidery

--- src/test/java/daos/core/ProductDaoIT.java
+++ src/test/java/daos/core/ProductDaoIT.java
@@ -16,6 +16,7 @@ import config.TestsPersistenceConfig;
 import entities.core.AlarmType;
 import entities.core.Product;
 import wrappers.ArticleFilterWrapper;
+import wrappers.EmbroideryFilterWrapper;
 import wrappers.ProductFilterWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -126,6 +127,50 @@ public class ProductDaoIT {
         articleFront.setMaxWholesalePrice(new BigDecimal("1"));
         articleFront.setMinWholesalePrice(new BigDecimal("10"));
         assertEquals(0,articleDao.findArticlesByFilter(articleFront).size());
+    }
+    
+    
+    @Test
+    public void testFindEmbroideryByFilterNormal() {
+        EmbroideryFilterWrapper embroideryFront= new EmbroideryFilterWrapper ();
+        embroideryFront.setDescription("");
+        embroideryFront.setReference("");
+        embroideryFront.setMaxRetailPrice(new BigDecimal("0"));
+        embroideryFront.setMinRetailPrice(new BigDecimal("0"));
+        embroideryFront.setMaxColors(100);
+        embroideryFront.setMinColors(1);
+        embroideryFront.setMaxSquareMillimeters(100);
+        embroideryFront.setMinSquareMillimeters(1);
+        embroideryFront.setMaxStitches(10000);
+        embroideryFront.setMinStitches(1);
+        assertNotNull(embroideryDao.findEmbroideryByFilter(embroideryFront));
+    }
+    
+    @Test
+    public void testFindEmbroideryByFilterExrem() {
+        EmbroideryFilterWrapper embroideryFront= new EmbroideryFilterWrapper ();
+        embroideryFront.setDescription("");
+        embroideryFront.setReference("");
+        embroideryFront.setMaxRetailPrice(new BigDecimal("0"));
+        embroideryFront.setMinRetailPrice(new BigDecimal("0"));
+        embroideryFront.setMaxColors(0);
+        embroideryFront.setMinColors(1000000);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());
+        embroideryFront.setMaxColors(1);
+        embroideryFront.setMinColors(10);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());
+        embroideryFront.setMaxSquareMillimeters(0);
+        embroideryFront.setMinSquareMillimeters(1000000000);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());
+        embroideryFront.setMaxSquareMillimeters(1);
+        embroideryFront.setMinSquareMillimeters(10);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());
+        embroideryFront.setMaxStitches(0);
+        embroideryFront.setMinStitches(1000000000);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());   
+        embroideryFront.setMaxStitches(1);
+        embroideryFront.setMinStitches(10);
+        assertEquals(0,embroideryDao.findEmbroideryByFilter(embroideryFront).size());
     }
 
 }
