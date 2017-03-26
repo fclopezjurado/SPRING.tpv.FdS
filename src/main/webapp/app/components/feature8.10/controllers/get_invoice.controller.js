c38011875e3d318a0c72705fe71ac06dfd4eecad
Francisco López <fclopezjurado@gmail.com>
2017-03-26 23:12:29 +0200

    Fixes in "get_invoice" AngularJS controller to use "getTicketsByUserMobileService" AngularJS service correctly after the last changes in the service.

--- src/main/webapp/app/components/feature8.10/controllers/get_invoice.controller.js
+++ src/main/webapp/app/components/feature8.10/controllers/get_invoice.controller.js
@@ -7,6 +7,8 @@ angular.module("tpv").controller("GetInvoiceController",
 		const THERE_ARE_NOT_INVOICES_MESSAGE 	= "ADVERTENCIA. No existen facturas para el usuario seleccionado";
 		const INVOICE_OPTION_IS_EMPTY 			= "ERROR. Seleccione al menos una factura";
 		const THERE_IS_NOT_TICKET_MESSAGE 		= "ERROR. No existe ticket para la factura seleccionada";
+		const BAD_REQUEST_MESSAGE				= "ERROR en la petición";
+		const OK_RESPONSE_CODE					= 200;
 		
         var vm 								= this;
         vm.formError 						= null;
@@ -17,15 +19,27 @@ angular.module("tpv").controller("GetInvoiceController",
 		vm.invoices							= [];
         
         vm.getUsers = function () {
-        	var serverResponseBody;
-
-            serverResponseBody 	= angular.fromJson(getUsersService.getUsers());
-            vm.users 			= serverResponseBody.data;
-            
-            if (vm.users.length === 0) {
-            	vm.formError = THERE_ARE_NOT_USERS_MESSAGE;
-            	vm.disableGetInvoice = true;
-            }
+        	getUsersService.getUsers().then(function (serverResponse) {
+                var serverResponse = angular.fromJson(serverResponse);
+                
+                if (serverResponse.status === OK_RESPONSE_CODE) {
+                	vm.users = serverResponse.data.userList;
+                	
+                	if (vm.users.length === 0) {
+                    	vm.formError = THERE_ARE_NOT_USERS_MESSAGE;
+            			vm.disableGetInvoice = true;
+                    }
+                    else
+            			vm.disableGetInvoice = false;
+                }
+                else {
+                	vm.formError 			= BAD_REQUEST_MESSAGE;
+        			vm.disableGetInvoice 	= true;
+                }
+            }, function () {
+            	vm.formError 			= BAD_REQUEST_MESSAGE;
+    			vm.disableGetInvoice 	= true;
+            });
         };
         
         vm.getInvoices = function () {
