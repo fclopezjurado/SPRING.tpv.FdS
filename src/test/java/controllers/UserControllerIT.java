612273a4a78988141e46ff252176d0980d9ad829
Francisco López <fclopezjurado@gmail.com>
2017-03-28 01:20:29 +0200

    Fixes in "UserControllerIT" class for tests from "test.controllers" package to add "testGetByTicketReference" and "testGetByTicketReferenceWithWrongTicketReference" tests.

--- src/test/java/controllers/UserControllerIT.java
+++ src/test/java/controllers/UserControllerIT.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.apache.logging.log4j.LogManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +13,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import api.exceptions.NotFoundTicketReferenceException;
 import config.PersistenceConfig;
 import config.TestsControllerConfig;
 import config.TestsPersistenceConfig;
 import entities.users.Role;
+import wrappers.TicketsWrapper;
 import wrappers.UserWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -28,8 +33,13 @@ public class UserControllerIT {
 
     private static final String USER_EMAIL = "test@test.com";
 
+    private static final String INVALID_TICKET_REFERENCE = "wg_yXs1LJmSvNsld-aXBg27P1jA";
+
     @Autowired
     private UserController userController;
+
+    @Autowired
+    private TicketController ticketController;
 
     @Autowired
     private Environment environment;
@@ -65,5 +75,28 @@ public class UserControllerIT {
     @Test
     public void testUserExistsByEmailWrong() {
         assertFalse(userController.userExistsByEmail(USER_EMAIL));
+    }
+
+    @Test
+    public void testGetByTicketReference() {
+        TicketsWrapper ticketsWrapper = this.ticketController.findAll();
+
+        try {
+            UserWrapper user = this.userController.getByTicketReference(ticketsWrapper.getFirstTicket().getReference());
+            assertEquals(ticketsWrapper.getFirstTicket().getUser().getMobile(), user.getMobile());
+        } catch (NotFoundTicketReferenceException exception) {
+            LogManager.getLogger(this.getClass()).info("testGetByTicketReference was wrong:  " + exception.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetByTicketReferenceWithWrongTicketReference() {
+        try {
+            this.userController.getByTicketReference(INVALID_TICKET_REFERENCE);
+            fail();
+        } catch (NotFoundTicketReferenceException exception) {
+            assertTrue(true);
+        }
     }
 }
