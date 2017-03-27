c1f30c452d7d51c89f410cde565dec41ed60dd8c
Enrique Arranz <enrique.arranz@urjc.es>
2017-03-27 22:33:51 +0200

    conectada la api con el front.end a falta de mejoras y el editar

--- src/main/webapp/app/components/feature09/cashier_balancing.controller.js
+++ src/main/webapp/app/components/feature09/cashier_balancing.controller.js
@@ -1,31 +1,39 @@
-angular.module("tpv").controller(
-		"CashierBalancingController",
-    function ($location, newCashierBalancingService, getBalancesService) {
-			"use strict";
+angular.module("tpv").controller("CashierBalancingController",
+    function ($location, $filter, newCashierBalancingService, getBalancesService) {
+		"use strict";
 
-			var vm = this;
+		var vm = this;
 
-			vm.balances = getBalancesService.findAll();
+		vm.balances = [];
 
-			vm.successList = true;
-			vm.showAlert = false;
-			vm.alertMessage = '';
+		vm.successList = false;
+		vm.showAlert = false;
+		vm.alertMessage = '';
 
-			vm.cashierbalancing = {
-				date : new Date(),
-				totalMoney : 50
-			};
+		vm.cashierbalancing = {
+			date : new Date(),
+			totalMoney : 50
+		};
 
-			vm.createCashierBalancing = function() {
-				vm.balances.push(newCashierBalancingService
-						.create(vm.cashierbalancing));
+		vm.createCashierBalancing = function() {
+			vm.cashierbalancing.date = $filter('date')(vm.cashierbalancing.date, "dd-MM-yyyy");
+			vm.balances.push(newCashierBalancingService.create(vm.cashierbalancing));
 
-				console.log(vm.balances);
-                $location.path("/feature09/cierrecaja");
+			//console.log(vm.balances);
+            //$location.path("/feature09/cierrecaja");
 
-			}
+		}
 
-			vm.getBalances = function() {
-				return [];
-			}
-		});
+		vm.getBalances = function() {
+			return vm.requestToGetBalances(getBalancesService);
+		}
+		
+		vm.requestToGetBalances = function (getBalancesService) {            
+            angular.fromJson(getBalancesService.findAll().then(function(result){
+            	vm.balances			= result;
+            	vm.successList		= true;
+            }));            
+        }
+		
+		vm.getBalances();
+	});
