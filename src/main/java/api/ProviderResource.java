54e60b677a38413d6ff4c63a0e355e26138f1ab1
Javier Molina <j.sheerp@gmail.com>
2017-03-28 02:10:15 +0200

    Secularizado el acceso a la creación, borrado y actualización de proveedores

--- src/main/java/api/ProviderResource.java
+++ src/main/java/api/ProviderResource.java
@@ -2,6 +2,7 @@ package api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ public class ProviderResource {
     }
 
     @RequestMapping(value = Uris.PROVIDERS, method = RequestMethod.POST)
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMIN')")
     public ProviderWrapper providerRegistration(@RequestBody ProviderWrapper providerWrapper)
             throws MalformedHeaderException, AlreadyExistProviderFieldException {
         validateFields(providerWrapper);
@@ -43,11 +45,13 @@ public class ProviderResource {
     }
 
     @RequestMapping(value = Uris.PROVIDERS, method = RequestMethod.PUT)
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMIN')")
     public ProviderWrapper providerUpdate(@RequestBody ProviderWrapper providerWrapper) throws NotFoundProviderIdException {
         ProviderWrapper wrapper = providerController.editProvider(providerWrapper);
         return wrapper;
     }
 
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMIN')")
     @RequestMapping(value = Uris.PROVIDERS+Uris.ID, method = RequestMethod.DELETE)
     public void providerDelete(@PathVariable(value = "id") String id) throws ProviderWithArticlesException {
         providerController.delete(id);
