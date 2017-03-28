305a76ddedf673fe7570050bbedb0eb6e69746d6
eneas <emacias@ces.vocento.com>
2017-03-28 21:50:57 +0200

    populate db service done

--- src/main/java/services/PopulateDbFromFiles.java
+++ src/main/java/services/PopulateDbFromFiles.java
@@ -5,30 +5,131 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 
+import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
+import com.esotericsoftware.yamlbeans.scalar.ScalarSerializer;
 
+import daos.core.AlarmDao;
+import daos.core.ArticleDao;
+import daos.core.EmbroideryDao;
+import daos.core.InvoiceDao;
+import daos.core.ProviderDao;
+import daos.core.TextilePrintingDao;
+import daos.core.TicketDao;
+import daos.core.VoucherDao;
+import daos.users.AuthorizationDao;
+import daos.users.TokenDao;
+import daos.users.UserDao;
+import entities.core.Alarm;
+import entities.core.Article;
+import entities.core.Provider;
+
+@Service
+@Transactional
 public class PopulateDbFromFiles {
 
-	public static ClassPathResource PATH = new ClassPathResource("fixtures");
+	public static String PATH = new String("fixtures");
 	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private AuthorizationDao authorizationDao;
+	
+	@Autowired
+	private TokenDao tokenDao;
+	
+	@Autowired
+	private VoucherDao voucherDao;
+	
+	@Autowired
+	private ProviderDao providerDao;
+	
+	@Autowired
+	private ArticleDao articleDao;
+	
+	@Autowired
+	private EmbroideryDao embroideryDao;
+	
+	@Autowired
+	private TextilePrintingDao textilePrintingDao;
+	
+	@Autowired
+	private TicketDao ticketDao;
+	
+	@Autowired
+	private InvoiceDao invoiceDao;
+	
+	@Autowired
+	private AlarmDao alertDao;
+	
+	public TreeMap<Integer, String> getFilesOrdered()
+	{
+		TreeMap<Integer, String> files = new TreeMap<Integer, String>();
+		files.put(1, "entities_core_alarm.yaml");
+		files.put(1, "entities_core_article.yaml");
+		files.put(1, "entities_core_cashierbalance.yaml");
+		files.put(1, "entities_core_embroidery.yaml");
+		files.put(1, "entities_core_invoice.yaml");
+		files.put(1, "entities_core_product.yaml");
+		files.put(0, "entities_core_provider.yaml");
+		files.put(1, "entities_core_shopping.yaml");
+		files.put(1, "entities_core_textileprinting.yaml");
+		files.put(1, "entities_core_ticket.yaml");
+		files.put(1, "entities_core_voucher.yaml");
+		files.put(1, "entities_users_authorization.yaml");
+		files.put(1, "entities_users_encrypting.yaml");
+		files.put(0, "entities_users_user.yaml");
+		
+		return files;
+		
+	}
 	public void readFromPath() throws IOException
 	{
-		File folder = PATH.getFile();
-		File[] listOfFixtures = folder.listFiles();
-		
 		String allFixturesTogether = new String();
-		for(int i=0; i<listOfFixtures.length; i++)
+
+		for(String file : getFilesOrdered().values())
 		{
-			allFixturesTogether += readFile(listOfFixtures[i]);
+			ClassPathResource resource = new ClassPathResource(PATH + "/" +file);
+			allFixturesTogether += readFile(resource.getFile());
 		}
-		
+
 		YamlReader reader = new YamlReader(allFixturesTogether);
-		Object fixtures = reader.read();
-		System.out.println(fixtures);
+		reader.getConfig().setScalarSerializer(BigDecimal.class, getBigDecimalSerializer());
+		reader.getConfig().setScalarSerializer(Calendar.class, getCalendarSerializer());
+		reader.getConfig().setPrivateFields(true);
+		
+		Map map = (Map)reader.read();
+		
+        userDao.save((List)map.get("users"));
+        authorizationDao.save((List)map.get("authorizations"));
+        tokenDao.save((List)map.get("tokens"));
+        voucherDao.save((List)map.get("vouchers"));
+        providerDao.save((List)map.get("providers"));
+        articleDao.save((List)map.get("articles"));
+        embroideryDao.save((List)map.get("embroiderys"));
+        textilePrintingDao.save((List)map.get("textilePrinting"));
+        ticketDao.save((List)map.get("textilesPrinting"));
+        invoiceDao.save((List)map.get("invoices"));
+        alertDao.save((List)map.get("alerts"));		
 	}
 	
 	private String readFile(File file) throws IOException
@@ -50,5 +151,46 @@ public class PopulateDbFromFiles {
 		}
 		
 		return everything;
+	}
+	
+	private ScalarSerializer<BigDecimal> getBigDecimalSerializer()
+	{
+		return new ScalarSerializer<BigDecimal>() {
+			@Override
+			public String write(BigDecimal object) throws YamlException {
+				return null;
+			}
+
+			@Override
+			public BigDecimal read(String value) throws YamlException {
+				BigDecimal decimal = BigDecimal.valueOf(Double.valueOf(value));
+				return decimal;
+			}
+			
+		};
+	}
+	
+	private ScalarSerializer<Calendar> getCalendarSerializer()
+	{
+		return new ScalarSerializer<Calendar>() {
+
+			@Override
+			public String write(Calendar object) throws YamlException {
+				return null;
+			}
+
+			@Override
+			public Calendar read(String value) throws YamlException {
+				SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy");
+				Calendar calendar = Calendar.getInstance();
+				try {
+					calendar.setTime(sdf.parse(value));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				return calendar;
+			}
+			
+		};
 	}
 }
--- src/test/java/services/PopulateDbFromFilesTest.java
+++ src/test/java/services/PopulateDbFromFilesTest.java
@@ -4,22 +4,42 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import config.PersistenceConfig;
+import config.TestsPersistenceConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { PersistenceConfig.class, TestsPersistenceConfig.class })
 
 public class PopulateDbFromFilesTest {
-	
+
+	@Autowired
 	private PopulateDbFromFiles populateDbFromFiles;
 
+	@Autowired
+	private DataService dataService;
+
 	@Before
-	public void setUp()
-	{
-		populateDbFromFiles =new PopulateDbFromFiles();
+	public void setUp() {
+		dataService.deleteAllExceptAdmin();
 	}
 
 	@Test
 	public void testReadFromPath() throws IOException {
 		populateDbFromFiles.readFromPath();
+	}
+
+	@After
+	public void tearDown() {
+		dataService.deleteAllExceptAdmin();
 	}
 
 }
