23958b4531052742c4ffc1a7b892a5928ea1a3d7
eduardofranco <eduardofranco@localhost>
2017-03-28 22:35:10 +0200

    Seguridad + test de los apis

--- src/test/java/api/TextilePrintingResourceFunctionalTesting.java
+++ src/test/java/api/TextilePrintingResourceFunctionalTesting.java
@@ -1,27 +1,50 @@
 package api;
 
-import org.junit.Test;
-import wrappers.ProductsOutFilterWrapper;
-import wrappers.TextilePritingFilterWrapper;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.client.HttpClientErrorException;
+
+import wrappers.ProductsOutFilterWrapper;
+import wrappers.TextilePritingFilterWrapper;
 
 public class TextilePrintingResourceFunctionalTesting {
+    
+    private String token;
+
+    @Before
+    public void seedDataBase() {
+        new RestService().deleteAll();
+        new RestService().seedDatabase();
+        token = new RestService().loginAdmin();
+    }
+
+    @After
+    public void after() {
+        new RestService().deleteAll();
+    }
     @Test
-    public void testGetTextilePrintingByFilterMock() {
+    public void testGetTextilePrintingByFilter() {
         TextilePritingFilterWrapper textilePrintingFilterWrapper = new TextilePritingFilterWrapper();
-        textilePrintingFilterWrapper.setDescription("descripcion");
-        textilePrintingFilterWrapper.setReference("reference");
+        textilePrintingFilterWrapper.setDescription("");
+        textilePrintingFilterWrapper.setReference("");
         textilePrintingFilterWrapper.setMinRetailPrice(new BigDecimal("0"));
         textilePrintingFilterWrapper.setMaxRetailPrice(new BigDecimal("0"));
-        textilePrintingFilterWrapper.setType("type");
-        List<ProductsOutFilterWrapper> productosSalidaMock = Arrays
-                .asList(new RestBuilder<ProductsOutFilterWrapper[]>(RestService.URL).path(Uris.TEXTILE_PRINTING + Uris.FILTER+Uris.MOCK)
-                        .clazz(ProductsOutFilterWrapper[].class).body(textilePrintingFilterWrapper).post().build());
-        assertEquals(1, productosSalidaMock.size());
+        textilePrintingFilterWrapper.setType("");
+        List<ProductsOutFilterWrapper> productosSalida = Arrays
+                .asList(new RestBuilder<ProductsOutFilterWrapper[]>(RestService.URL).path(Uris.TEXTILE_PRINTING + Uris.FILTER)
+                        .clazz(ProductsOutFilterWrapper[].class).body(textilePrintingFilterWrapper).basicAuth(token, "").post().build());
+        assertTrue(productosSalida.size()>0);
+    }
+    
+    @Test(expected=HttpClientErrorException.class)
+    public void testGetTextilePrintingByFilterException() {
+        
     }
 }
